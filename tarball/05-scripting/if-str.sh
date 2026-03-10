b8cf132e5e0936b963425acf4019abca0ef413d1
anon <>
2026-03-11 00:04:50 +0800

    docs(scripting): cleanup string testing

--- tarball/05-scripting/if-str.sh
+++
@@ -1,7 +0,0 @@
-str="something"
-if [[ -z $str ]]; then
-    echo "str is empty"
-elif [[ $str = 'something' ]]; then
-    # = and == are both ok
-    echo "str is 'something'"
-fi
---
+++ tarball/05-scripting/if-test-str.sh
@@ -0,0 +1,4 @@
+str="something"
+if [[ -z $str ]]; then
+    echo "str is empty"
+fi
