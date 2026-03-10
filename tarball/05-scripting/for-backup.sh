4e2caa6fea01e3fb021d3711bc6595c1886bdfaa
anon <>
2026-03-11 00:38:36 +0800

    docs(scripting): fix for loops

--- tarball/05-scripting/for-backup.sh
+++ tarball/05-scripting/for-backup.sh
@@ -1,6 +1,3 @@
-count=0
-for file in $(ls); do
+for file in *; do
     cp $file $file.backup
-    count=$(( count + 1 ))
 done
-echo "$count files backed up"
---
+++ tarball/05-scripting/for-minimal.sh
@@ -0,0 +1,3 @@
+for name in alice bob charlie; do
+    echo "My name is $name."
+done
