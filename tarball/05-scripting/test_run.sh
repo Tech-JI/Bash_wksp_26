37bc064647b3ae749d8a56ebfe0d6095c30eea5d
sherryshijr <sherryshijr@sjtu.edu.cn>
2026-03-10 08:20:33 +0800

    Add bash scripts and gitattributes

---
+++ tarball/05-scripting/test_run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "========================================="
+echo "   Start executing"
+export MY_MAGIC_VAR="1234"
+cd ../
+echo "   End executing"
+echo "========================================="
